e4671d409bf40c1d9a647bbf002a161ff01d2950
Abionics <abionics.dev@gmail.com>
2022-05-26 16:30:36 +0300

    Improve mock type, add constants

--- common/api/mock_request.py
+++ common/api/mock_request.py
@@ -1,9 +1,13 @@
 from collections import namedtuple
+from typing import cast
+
+from fastapi import Request
 
 MockRequestClient = namedtuple('MockRequestClient', 'host')
 MockRequest = namedtuple('MockRequest', 'client')
 
 
-def create_mock_request() -> MockRequest:
+def create_mock_request() -> Request:
     client = MockRequestClient(None)
-    return MockRequest(client)
+    request = MockRequest(client)
+    return cast(Request, request)
--- common/constants.py
+++ common/constants.py
@@ -1,4 +1,9 @@
-UNKNOWN_ACCOUNT_NAME = '<unknown>'
+# common
+SERVICES = ('decoder', 'detector', 'loader')
+
+# decoder
 UNKNOWN_FUNCTION_NAME = '<unknown>'
 
-SERVICES = ('decoder', 'detector', 'loader')
+# detector
+UNKNOWN_ACCOUNT_NAME = '<unknown>'
+NEVER_EXISTS_ACCOUNT_NAME = '<NeverExists>'
