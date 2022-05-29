b679abb9872b5c912bfae126d20ae2e971694bb2
Abionics <abionics.dev@gmail.com>
2022-05-29 16:56:20 +0300

    Add detect special method

---
+++ common/detector/detect_special.py
@@ -0,0 +1,12 @@
+from tvmbase.models.tvm.account import Account
+
+from common.constants import NEVER_EXISTS_ACCOUNT_NAME
+from common.detector.response.detected import Detected
+
+
+def detect_special(account: Account) -> Detected:
+    if account.data is None:
+        return Detected.from_special(NEVER_EXISTS_ACCOUNT_NAME)
+    if account.data.code_hash is None:
+        name = '<' + account.data.acc_type_name + '>'
+        return Detected.from_special(name)
