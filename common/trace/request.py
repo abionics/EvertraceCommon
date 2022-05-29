383d1cfa3d05dbfff86f53750610d8e10a41b69f
Abionics <abionics.dev@gmail.com>
2022-05-29 22:04:43 +0300

    add default trace net

--- common/trace/request.py
+++ common/trace/request.py
@@ -5,7 +5,7 @@ from common.loader.request.request import ExtractorClass
 
 class TraceServerParam(BaseModel):
     idx: str
-    net: str
+    net: str = 'main'
     extractor_class: ExtractorClass = ExtractorClass.TINY
     target: str | None = None
     sort: bool = True
