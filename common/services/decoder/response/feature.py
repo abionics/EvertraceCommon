4d6c12dcf75b29a9d296a8025622a73ecdc5dc12
Abionics <abionics.dev@gmail.com>
2022-06-03 11:40:41 +0300

    Rename features

--- common/services/decoder/response/feature.py
+++ common/services/decoder/response/feature.py
@@ -4,8 +4,9 @@ from enum import Enum
 class Feature(str, Enum):
     SPECIAL = 'special'
     USER_ABI = 'user abi'
+    DATABASE_CONTRACT = 'database contract'
+    DATABASE_ABI = 'database abi'
     KNOWN_FUNCTION = 'known function'
-    KNOWN_ABI = 'known abi'
     BRUTEFORCE = 'bruteforce'
     NONAME = 'noname'
     UNKNOWN = 'unknown'
--- common/services/detector/response/feature.py
+++ common/services/detector/response/feature.py
@@ -3,8 +3,8 @@ from enum import Enum
 
 class Feature(str, Enum):
     SPECIAL = 'special'
-    USER_ABI = 'user_abi'
-    ADDRESS = 'address'
-    HASH = 'hash'
-    KNOWN_ABI = 'known_abi'
+    USER_ABI = 'user abi'
+    DATABASE_ADDRESS = 'database address'
+    DATABASE_HASH = 'database hash'
+    DATABASE_ABI = 'database abi'
     UNKNOWN = 'unknown'
