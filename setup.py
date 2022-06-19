621c874e77db4b3efc4ccb05988109a3bee06552
Abionics <abionics.dev@gmail.com>
2022-06-19 14:51:53 +0300

    Update version of tvmbase

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     ],
     packages=find_packages(exclude=['tests', 'examples']),
     install_requires=[
-        'tvmbase>=2.3.1',
+        'tvmbase>=2.3.4',
         'loguru>=0.6.0',
         'aiohttp>=3.8.1',
         'sqlalchemy>=1.4.36',
